65e82cc333430bcb27b8fccd9375046e7a2baf39
yogasindhu <yogasindhu1101@gmail.com>
2022-08-15 21:21:37 +0530

    Create yoga murali project.py

---
+++ yoga murali project.py
@@ -0,0 +1,19 @@
+# program to check Armstrong number in a certian interval
+
+lower =100
+upper=2000
+
+for num in range(lower,upper+ 1):
+    # order of number
+    order = len(str(num))
+
+    #inititalize sum
+    sum=0
+
+    temp=  num
+    while temp >0:
+        digits =temp % 10
+        sum +=digits ** order
+        tem
+          print(num)
+        
